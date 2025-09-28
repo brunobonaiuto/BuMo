0595900cad229f452adf0e940b5854f81030f485
Bruno Bonaiuto <bonaiutom60@gmail.com>
2025-09-28 19:12:29 +0200

    seme random stuff

--- app/src/main/java/com/bonaiuto/bumo/MainActivity.java
+++ app/src/main/java/com/bonaiuto/bumo/MainActivity.java
@@ -1,24 +1,49 @@
 package com.bonaiuto.bumo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d("fail", "nothing failed");
+    }
+
+    public void handleInputText(View view){
+        EditText viewById = findViewById(R.id.sourceName);
+        String input = viewById.getText().toString();
+        TextView viewById1 = findViewById(R.id.outputText);
+        viewById1.setVisibility(View.VISIBLE);
+        viewById1.setText(String.format("The input was: %s", input));
+        Toast.makeText(this, "alert", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void disable(View v) {
+
+
+        v.setEnabled(false);
+        Log.d("ok", "message");
+        if (v instanceof Button) {
+            Button v1 = (Button) v;
+            v1.setText("Hola morena bella");
+        }
+
+        TextView viewById = findViewById(R.id.hello);
+        viewById.setText("oh no, you are moren\naaaa");
+    }
+
+    public void nextPage(View view) {
+
     }
 }
